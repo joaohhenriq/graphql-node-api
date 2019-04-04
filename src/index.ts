8e13c2c28ddbb0457e5b01db1f7ffaf27009533e
joaoh <joao_camargo10@hotmail.com>
2019-04-04 19:57:31 -0300

    Configurando express para o projeto

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import app from './app';
+import * as http from 'http';
+
+
+const server = http.createServer(app);
+
+server.listen(3000);
+server.on('listening', () => console.log('Listening on port 3000...'));
